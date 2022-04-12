baa24707bb2d70501e01a1915f0c70088af72123
Suyog Joshi <suyogjoshi@Suyogs-MacBook-Pro.local>
2022-04-12 15:34:52 -0700

    running weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const [city, setCity] = useState("");
+  // const [currentLocation, setCurrentLocation] = useState([]);
+
+  const searchCity = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93eee144b7450e33ce666ddbb913584a`
+        )
+        .then((response) => {
+          console.log(response.data);
+          setData(response.data);
+        });
+      setCity(" ");
+    }
+  };
+
+  function degree(temp) {
+    const result = (temp - 273.15) * 1.8 + 32;
+    return result.toFixed(2);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="heading">
+        <h1>Weather</h1>
+      </div>
+      <div className="search">
+        <input
+          className="search-box"
+          type="text"
+          value={city}
+          placeholder="Enter City"
+          onChange={(event) => setCity(event.target.value)}
+          onKeyPress={searchCity}
+        />
+      </div>
+      <div className="city">
+        <h2>{data.name}</h2>
+      </div>
+
+      <div className="temp">
+        {data.main ? <h1>{degree(data.main.temp)} F</h1> : null}
+      </div>
+      <div className="temp-description">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.main ? (
+            <b>
+              {degree(data.main.feels_like)} <br /> Feels like
+            </b>
+          ) : null}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          {data.main ? (
+            <b>
+              Wind speed: <br /> {degree(data.wind.speed)} mph
+            </b>
+          ) : null}
+        </p>
+        <p>
+          {data.weather ? (
+            <b>
+              Weather: <br /> {degree(data.weather[0].description)}
+            </b>
+          ) : null}
+        </p>
+      </div>
     </div>
   );
 }
